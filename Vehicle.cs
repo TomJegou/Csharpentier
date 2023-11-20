e1748192fc09b12ab09a35b9a3a84940b317ee6c
TomJegou <tomjegou10@gmail.com>
2023-11-20 10:18:09 +0100

    basic implementation

--- Vehicle.cs
+++ Vehicle.cs
@@ -1,9 +1,26 @@
 using System;
+using System.ComponentModel;
 
 namespace Vehicle
 {
-    class Vehicle
+    class Vehicle(string brand, string honkSong) : IVehicle
     {
-        
+        public string Brand { get; set; } = brand;
+        public string HonkSong { get; set; } = honkSong;
+
+        public void Go()
+        {
+            Console.WriteLine("The Vehicle is goigng in the intersection");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("The vehicle has stopped");
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine(HonkSong);
+        }
     }
 }
