6e6355cc079343518fd14c24a8b972291b88e32a
TomJegou <tomjegou10@gmail.com>
2023-11-20 11:07:49 +0100

    add Speed properties, and override implement ToString()

--- Vehicle.cs
+++ Vehicle.cs
@@ -3,10 +3,11 @@ using System.ComponentModel;
 
 namespace Vehicle
 {
-    public class Vehicle(string brand, string honkSong) : IVehicle
+    public class Vehicle(string brand, int speed,string honkSong) : IVehicle
     {
         public string Brand { get; set; } = brand;
         public string HonkSong { get; set; } = honkSong;
+        public int Speed { get; set; } = speed;
 
         public void Go()
         {
@@ -21,6 +22,11 @@ namespace Vehicle
         public void Honk()
         {
             Console.WriteLine(HonkSong);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Vehicle :\n{0}\n{1}", Brand, Speed);
         }
     }
 }
