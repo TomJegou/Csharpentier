03e2402062fc380446dc3d885f0fe8356650febd
TomJegou <tomjegou10@gmail.com>
2023-11-20 10:18:00 +0100

    add properties + method Honk

--- IVehicle.cs
+++ IVehicle.cs
@@ -1,7 +1,10 @@
 namespace Vehicle
 {
     public interface IVehicle{
+        public string Brand {get; set;}
+        public string HonkSong { get; set; }
         public void Go();
         public void Stop();
+        public void Honk();
     }
 }
