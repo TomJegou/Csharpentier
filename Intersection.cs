8a348dd0336730791028bd954b40aa37d0e847ed
TomJegou <tomjegou10@gmail.com>
2023-11-20 11:27:07 +0100

    implement ToString()

--- Intersection.cs
+++ Intersection.cs
@@ -1,3 +1,7 @@
+using System.Collections.Specialized;
+using System.IO;
+using Microsoft.VisualBasic;
+
 namespace Intersection
 {
     public class Intersection(int nbrRoads, int nbrCrossWalk): IIntersection
@@ -5,9 +9,12 @@ namespace Intersection
         public int NbrRoads { get; set; } = nbrRoads;
         public int NbrCrossWalk { get; set; } = nbrCrossWalk;
 
-        public override string ToString()
+        public override string? ToString()
         {
-            return "";
+            using FileStream fs = File.OpenRead("assets/Intersection.txt");
+            byte[] buf = new byte[1048];
+            fs.Read(buf);
+            return Convert.ToString(buf);
         }
     } 
 }
