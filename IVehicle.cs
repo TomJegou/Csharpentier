d948acd861fbf2d749682bcef1df23cc1913951a
TomJegou <tomjegou10@gmail.com>
2023-11-20 18:46:17 +0100

    add 2 properties for position in interface

--- IVehicle.cs
+++ IVehicle.cs
@@ -1,6 +1,10 @@
+using Intersection;
+
 namespace Vehicle
 {
     public interface IVehicle{
+        public ListDirection StartingPoint { get; set; }
+        public ListDirection DestinationPoint { get; set; }
         public string Brand {get; set;}
         public int Speed { get; set; }
         public string HonkSong { get; set; }
