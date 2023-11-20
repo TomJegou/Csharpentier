62d9526e5f22f42d28fc9f15436a17e8fc9ab0c4
Tom Saillard <tom.saillard@ynov.com>
2023-11-20 11:05:05 +0100

    Done the method to identify what type of person it is

--- Person.cs
+++ Person.cs
@@ -1,14 +1,35 @@
-class NPC : Person
-
-    public NPC(string age, )
+public class NPC 
+{
+    public NPC(int age, int vitesse )
     {
         this.Age = age;
+        this.Vitesse = vitesse;
     }
 
-    
-public enum StadeDeVie
+    public enum StadeDeVie
 {
     Enfant,
     Adulte,
     Vieux
 }
+
+public StadeDeVie ClasserParAge()
+{
+    if (Age < 18)
+    {
+        return StadeDeVie.Enfant;
+    }
+    else if (Age >= 18 && Age < 60)
+    {
+        return StadeDeVie.Adulte;
+    }
+    else
+    {
+         return StadeDeVie.Vieux;
+    }
+}
+
+}
+
+
+
