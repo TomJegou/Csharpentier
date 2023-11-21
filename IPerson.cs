68e950cbfc0db5ebe184da0a21d50bdae33ce22f
TomJegou <tomjegou10@gmail.com>
2023-11-21 08:33:22 +0100

    fix format

--- IPerson.cs
+++ IPerson.cs
@@ -1,6 +1,7 @@
 namespace Person
 {
-    public interface IPerson{
+    public interface IPerson
+    {
         public int Age { get; set; }
         public int Vitesse { get; set; }
         public StadeDeVie ClasserParAge();
