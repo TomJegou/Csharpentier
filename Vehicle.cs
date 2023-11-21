130c19684f2dfa9ef2fe4dc354606f32e2874ef0
okylian <kylian.oger@ynov.com>
2023-11-21 08:31:11 +0100

    add Type Vehicle

--- Vehicle.cs
+++ Vehicle.cs
@@ -4,11 +4,12 @@ using Intersection;
 
 namespace Vehicle
 {
-    public class Vehicle(ListBrands brand, int speed,string honkSong, ListDirection startingPoint, ListDirection destinationPoint) : IVehicle
+    public class Vehicle(ListBrands brand, int speed,string honkSong, ListDirection startingPoint, ListDirection destinationPoint, ListVehicle typeVehicle) : IVehicle
     {
         public ListDirection StartingPoint { get; set; } = startingPoint;
         public ListDirection DestinationPoint { get; set; } = destinationPoint;
         public ListBrands Brand { get; set; } = brand;
+        public ListVehicle TypeVehicle {get; set; } = typeVehicle;
         public string HonkSong { get; set; } = honkSong;
         public int Speed { get; set; } = speed;
 
@@ -29,7 +30,7 @@ namespace Vehicle
 
         public override string ToString()
         {
-            return string.Format("Vehicle :\n{0}\n{1}", Brand, Speed);
+            return string.Format("Vehicle :\n{0}\n{1}\n{2}", TypeVehicle, Brand, Speed);
         }
     }
 }
