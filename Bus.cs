70e38929dc7cd662c69154a4ad5849b25dfafbc5
TomJegou <tomjegou10@gmail.com>
2023-11-21 08:11:36 +0100

    cleaning

--- Bus.cs
+++ Bus.cs
@@ -1,20 +1,11 @@
+using Intersection;
+
 namespace Vehicle
 {
     public class Bus : Vehicle
     {
-
-
-    public Bus(ListDirection startingPoint, ListDirection destinationPoint, ListBrands brand, int speed, string honkSong) : base(startingPoint, destinationPoint, brand, honkSong,speed)
-    {
+        public Bus(ListDirection startingPoint, ListDirection destinationPoint, ListBrands brand, int speed, string honkSong) : base(startingPoint, destinationPoint, brand, honkSong,speed)
+        {
+        }
     }
-
-
-    public int GetSpeed {
-        get { return this.speed; }
-    }
-
-    }
-
-
 }
-
