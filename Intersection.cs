8890cc527a9dbe6c4a8c1e5b507804faa99da2fe
TomJegou <tomjegou10@gmail.com>
2023-11-21 10:15:39 +0100

    create function generate

--- Intersection.cs
+++ Intersection.cs
@@ -1,18 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Vehicle;
+
 namespace Intersection
 {
-    public class Intersection(): IIntersection
+    public class Intersection(int nbrRoads, int nbrCrossWalk): IIntersection
     {
-        public int NbrRoads { get; set; }
-        public int NbrCrossWalk { get; set; }
+        public int NbrRoads { get; set; } = nbrRoads;
+        public int NbrCrossWalk { get; set; } = nbrCrossWalk;
         public List<Road> ListRoads { get; set; } = [];
-        
-        public Intersection(int nbrRoads, int nbrCrossWalk)
-        {
-            NbrRoads = nbrRoads;
-            NbrCrossWalk = nbrCrossWalk;
-            Generate();
-
-        }
 
         public override string? ToString()
         {
@@ -21,9 +19,16 @@ namespace Intersection
 
         public void Generate()
         {
-            for (int i  = 0; i <= NbrRoads; i++)
+            for (int i  = 0; i < NbrRoads; i++)
             {
                 List<Vehicle.Vehicle> lV = [];
+                int nbrV = new Random().Next(0, 3);
+                for (int j = 0; j < nbrV; j++)
+                {
+                    int sP = new Random().Next(0, ListRoads.Count);
+                    int dP = new Random().Next(0, ListRoads.Count);
+                    lV.Add(new Vehicle.Vehicle(ListBrands.BMW, 300, "TUUT TUUT !", ListRoads[sP], ListRoads[dP], ListVehicle.Car));
+                }
                 Road road = new(lV, string.Format("{0}", i));
                 ListRoads.Add(road);
             }
