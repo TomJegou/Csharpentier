b6dd03042de9d8f44270ca4aa6981995c432cfa3
TomJegou <tomjegou10@gmail.com>
2023-11-21 09:57:51 +0100

    refactoring

--- Intersection.cs
+++ Intersection.cs
@@ -1,17 +1,32 @@
-using System.Collections.Specialized;
-using System.IO;
-using Microsoft.VisualBasic;
-
 namespace Intersection
 {
-    public class Intersection(int nbrRoads, int nbrCrossWalk): IIntersection
+    public class Intersection(): IIntersection
     {
-        public int NbrRoads { get; set; } = nbrRoads;
-        public int NbrCrossWalk { get; set; } = nbrCrossWalk;
+        public int NbrRoads { get; set; }
+        public int NbrCrossWalk { get; set; }
+        public List<Road> ListRoads { get; set; } = [];
+        
+        public Intersection(int nbrRoads, int nbrCrossWalk)
+        {
+            NbrRoads = nbrRoads;
+            NbrCrossWalk = nbrCrossWalk;
+            Generate();
+
+        }
 
         public override string? ToString()
         {
             return File.ReadAllText("assets/Intersection.txt");
+        }
+
+        public void Generate()
+        {
+            for (int i  = 0; i <= NbrRoads; i++)
+            {
+                Vehicle.Vehicle[] lV = [];
+                Road road = new(lV, string.Format("{0}", i));
+                ListRoads.Add(road);
+            }
         }
     }
 }
