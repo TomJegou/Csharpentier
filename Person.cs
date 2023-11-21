631986050b1d5cd4bbe68b21f4faadacf4de4f5a
L0uxe <Loulourxl@hotmail.com>
2023-11-21 07:58:17 +0100

    indentation

--- Person.cs
+++ Person.cs
@@ -11,25 +11,24 @@ namespace Person{
         public enum StadeDeVie{
         Enfant,
         Adulte,
-        Vieux  
+        Vieux,  
         }
         //Méthode pour détecter quel est le stade de vie du NPC
-    public StadeDeVie ClasserParAge()
-    {
-        if (Age < 18)
+        public StadeDeVie ClasserParAge()
         {
-            return StadeDeVie.Enfant;
+            if (Age < 18)
+            {
+                return StadeDeVie.Enfant;
+            }
+            else if (Age >= 18 && Age < 60)
+            {
+                return StadeDeVie.Adulte;
+            }
+            else
+            {
+                return StadeDeVie.Vieux;
+            }
         }
-        else if (Age >= 18 && Age < 60)
-        {
-            return StadeDeVie.Adulte;
-        }
-        else
-        {
-            return StadeDeVie.Vieux;
-        }
-    }
-
     }
 }
 
