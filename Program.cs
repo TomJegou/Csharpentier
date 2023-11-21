d623e93f3a2ac201b57b083aa7adfe342582847c
TomJegou <tomjegou10@gmail.com>
2023-11-21 11:16:51 +0100

    create infinite loop

--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@ namespace Program
     {
         public static void Main()
         {
-            Intersection.Intersection myIntersection = new(4, 4);
+            Intersection.Intersection myIntersection = new(3, 0);
             myIntersection.Generate();
             Console.WriteLine(myIntersection);
+            while (true)
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
