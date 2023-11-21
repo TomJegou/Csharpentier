0ba1cf2a5bdb5a511b06928efc215aa970348749
L0uxe <Loulourxl@hotmail.com>
2023-11-21 08:17:18 +0100

    rm enum from person.cs

--- Person.cs
+++ Person.cs
@@ -1,14 +1,10 @@
+using listStadeDeVie;
+
 namespace Person{
     public class NPC(int age, int vitesse)
     { 
         public int Age {get; set;} = age;
         public int Vitesse {get; set;} = vitesse;
-        //Enumération avec les 3 Stades de vie possible d'un NPC
-        public enum StadeDeVie{
-        Enfant,
-        Adulte,
-        Vieux,  
-        }
         //Méthode pour détecter quel est le stade de vie du NPC
         public StadeDeVie ClasserParAge()
         {
