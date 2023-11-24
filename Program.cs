d459f709c7d77e6686978e7f9a0a9ce0901db96c
kylian oger <kylian.oger@ynov.com>
2023-11-24 18:23:25 +0100

    push

--- Program.cs
+++ Program.cs
@@ -6,16 +6,20 @@ namespace Program
     {
         public static void Main()
         {
+            // Main loop
             while (true)
             {
                 Intersection.Intersection intersection = new(2, 0, true);
                 Console.WriteLine(intersection);
+                // Loop through each road
                 foreach (Road road in intersection.ListRoads)
                 {
+                    // Loop through each car in road
                     foreach (Vehicle.Vehicle vehicle in road.ListVehicle)
                     {
                         Console.WriteLine("Press any key to continue");
                         Console.ReadKey();
+
                         vehicle.Go();
                         road.ListVehicle.Remove(vehicle);
                     }
