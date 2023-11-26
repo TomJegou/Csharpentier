f23bf94f527d8f45b99334a7314dd5b0fa926914
TomJegou <tomjegou10@gmail.com>
2023-11-26 18:54:10 +0100

    create file

---
+++ Person.cs
@@ -0,0 +1,11 @@
+namespace NPC
+{
+    public class Humain : NPC
+    {
+        public Humain(int age, int vitesse) : base(age, vitesse)
+        {
+            Age = age;
+            Vitesse = vitesse;
+        }
+    }
+}
