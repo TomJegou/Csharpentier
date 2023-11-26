8eb0a029f1c87704b63fd5b291a2b67a5ee6f98c
TomJegou <tomjegou10@gmail.com>
2023-11-26 19:44:28 +0100

    refactoring ToString Fot humain and trafficlights

--- Road.cs
+++ Road.cs
@@ -1,32 +1,41 @@
-using TrafficLight;
-
 namespace Intersection
 {
     public class Road
     {
         public string RoadName { get; set; }
         public List<Vehicle.Vehicle> ListVehicle;
-
         public TrafficLight.TrafficLight? TrafficLight;
-
-        public Road(List<Vehicle.Vehicle> listVehicle, string roadName)
+        public List<NPC.Humain> ListHuman;
+        public Road(List<Vehicle.Vehicle> listVehicle, List<NPC.Humain> listHumain,string roadName)
         {
             RoadName = roadName;
             ListVehicle = listVehicle;
+            ListHuman = listHumain;
         }
 
         public override string ToString()
         {
             string result = string.Format("The road {0} contains : \n\n", RoadName);
-            if (ListVehicle.Count == 0)
+            if (ListVehicle.Count == 0 && ListHuman.Count == 0)
             {
                 result += "Nothing\n";
             } else
             {
-                foreach (Vehicle.Vehicle car in ListVehicle)
+                foreach(Vehicle.Vehicle car in ListVehicle)
                 {
                     result += car;
                 }
+            }
+            if (ListHuman.Count != 0)
+            {
+                foreach(NPC.Humain human in ListHuman)
+                {
+                    result += human;
+                }
+            }
+            if (TrafficLight != null)
+            {
+                result += "\n And a Traffic light";
             }
             return result;
         }
