062f426e5f6bf2bd7beb645846f11060fdfeace1
TomJegou <tomjegou10@gmail.com>
2023-11-26 19:14:23 +0100

    change properties NbrCrossWalk > NbrCrossWalkers

--- IIntersection.cs
+++ IIntersection.cs
@@ -3,7 +3,7 @@ namespace Intersection
     public interface IIntersection
     {
         public int NbrRoads { get; set; }
-        public int NbrCrossWalk { get; set; }
+        public int NbrCrossWalkers { get; set; }
         public void Generate();
         public List<Road> ListRoads { get; set; }
     }
