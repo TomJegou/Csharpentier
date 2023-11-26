f8063933ae9036dfe6ea056e64da04d68a0bca33
TomJegou <tomjegou10@gmail.com>
2023-11-26 19:37:16 +0100

    implement method Cross

--- Human.cs
+++ Human.cs
@@ -2,7 +2,7 @@ namespace NPC
 {
     public class Humain : NPC
     {
-        bool AsCrossed { get; set; }
+        public bool AsCrossed { get; set; } = false;
         public string Name { get; set; }
         public Humain(int age, int speed, string name) : base(age, speed)
         {
@@ -13,7 +13,13 @@ namespace NPC
 
         public override string ToString()
         {
-            return string.Format("Humain:\n*   {0}\n*   {1}\n*   {2}", Age, Speed, Name);
+            return string.Format("Humain:\n*   {0}\n*   {1}\n*   {2}\n", Age, Speed, Name);
+        }
+
+        public void CrossRoad()
+        {
+            Console.WriteLine(string.Format("Human : My name is {0} and I'm crossing the road !", Name));
+            AsCrossed = true;
         }
     }
 }
