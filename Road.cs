fc3071b212dea531bd7672e900700db5ace06214
L0uxe <Loulourxl@hotmail.com>
2023-11-27 08:27:14 +0100

    a

--- Road.cs
+++ Road.cs
@@ -2,41 +2,60 @@ namespace Intersection
 {
     public class Road
     {
+        // Name of the road
         public string RoadName { get; set; }
+
+        // List of vehicles on the road
         public List<Vehicle.Vehicle> ListVehicle;
+
+        // Traffic light on the road (nullable)
         public TrafficLight.TrafficLight? TrafficLight;
+
+        // List of pedestrians (humans) on the road
         public List<NPC.Human> ListHuman;
-        public Road(List<Vehicle.Vehicle> listVehicle, List<NPC.Human> listHuman,string roadName)
+
+        // Constructor for the Road class
+        public Road(List<Vehicle.Vehicle> listVehicle, List<NPC.Human> listHuman, string roadName)
         {
             RoadName = roadName;
             ListVehicle = listVehicle;
             ListHuman = listHuman;
         }
 
+        // Override of ToString method to display road details
         public override string ToString()
         {
             string result = string.Format("The road {0} contains : \n\n", RoadName);
+
+            // Check if the road has no vehicles or pedestrians
             if (ListVehicle.Count == 0 && ListHuman.Count == 0)
             {
                 result += "Nothing\n";
-            } else
+            }
+            else
             {
-                foreach(Vehicle.Vehicle car in ListVehicle)
+                // Display details of vehicles on the road
+                foreach (Vehicle.Vehicle car in ListVehicle)
                 {
-                    result += car;
+                    result += car; // Assuming Vehicle class has an overridden ToString method
                 }
             }
+
+            // Display details of pedestrians on the road if there are any
             if (ListHuman.Count != 0)
             {
-                foreach(NPC.Human human in ListHuman)
+                foreach (NPC.Human human in ListHuman)
                 {
-                    result += human;
+                    result += human; // Assuming Human class has an overridden ToString method
                 }
             }
+
+            // Check if there's a traffic light on the road and include it in the result
             if (TrafficLight != null)
             {
                 result += "\n And a Traffic light";
             }
+
             return result;
         }
     }
--- Vehicle.cs
+++ Vehicle.cs
@@ -1,18 +1,20 @@
-using Intersection;
+using Intersection; // Importing Intersection namespace to access the Road class
 
 namespace Vehicle
 {
     public class Vehicle : IVehicle
     {
-        public Road StartingPoint { get; set; }
-        public Road DestinationPoint { get; set; }
-        public Road CurrentPoint { get; set; }
-        public ListBrands Brand { get; set; }
-        public ListVehicle TypeVehicle {get; set; }
-        public string HonkSong { get; set; }
-        public int Speed { get; set; }
+        // Properties of the Vehicle class
+        public Road StartingPoint { get; set; } // Starting point of the vehicle
+        public Road DestinationPoint { get; set; } // Destination point of the vehicle
+        public Road CurrentPoint { get; set; } // Current point of the vehicle
+        public ListBrands Brand { get; set; } // Brand of the vehicle
+        public ListVehicle TypeVehicle { get; set; } // Type of vehicle
+        public string HonkSong { get; set; } // Sound made when honking
+        public int Speed { get; set; } // Speed of the vehicle
 
-        public Vehicle(ListBrands brand, int speed,string honkSong, Road startingPoint, Road destinationPoint, ListVehicle typeVehicle)
+        // Constructor for the Vehicle class
+        public Vehicle(ListBrands brand, int speed, string honkSong, Road startingPoint, Road destinationPoint, ListVehicle typeVehicle)
         {
             StartingPoint = startingPoint;
             DestinationPoint = destinationPoint;
@@ -23,25 +25,29 @@ namespace Vehicle
             Speed = speed;
         }
 
+        // Method for the vehicle to move towards its destination
         public void Go()
         {
             Console.WriteLine(string.Format("The {0} is moving to road {1}\n", TypeVehicle, DestinationPoint.RoadName));
             CurrentPoint = DestinationPoint;
         }
 
+        // Method to stop the vehicle
         public void Stop()
         {
             Console.WriteLine("The vehicle has stopped");
         }
 
+        // Method to make the vehicle honk
         public void Honk()
         {
             Console.WriteLine(HonkSong);
         }
 
+        // Override of ToString method to display vehicle details
         public override string ToString()
         {
-            return string.Format("Vehicle :\n*  {0}\n*  {1}\n*  {2}\n*  Start at road {3}\n*  Wants to go to road {4}\n\n", TypeVehicle, Brand, Speed, StartingPoint.RoadName, DestinationPoint.RoadName);
+            return string.Format("Vehicle :\n*  {0}\n*  {1}\n*  {2}\n*  Start at road {3}\n*  Wants to go to road {4}\n\n",TypeVehicle, Brand, Speed, StartingPoint.RoadName, DestinationPoint.RoadName);
         }
     }
 }
